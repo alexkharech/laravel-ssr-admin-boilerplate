393566fbed275053e95599a84c5c74bbce9eb3ed
alexkharech <41755348+alexkharech@users.noreply.github.com>
2019-12-04 11:23:40 +0300

    removing double code

--- webpack.mix.js
+++ webpack.mix.js
@@ -44,38 +44,3 @@ if (process.env.npm_lifecycle_event !== "hot") {
         extractVueStyles: true
     }).version();
 }
-const path = require("path");
-const mix = require("laravel-mix");
-
-mix.options({
-    postCss: [require("autoprefixer")],
-    hmrOptions: {
-        host: "localhost", // site's host name
-        port: 8080
-    }
-}).webpackConfig({
-    // add any webpack dev server config here
-    devServer: {
-        proxy: {
-            host: "localhost", // host machine ip
-            port: 8080
-        },
-        watchOptions: {
-            aggregateTimeout: 200,
-            poll: 5000
-        },
-        contentBase: path.resolve(__dirname, "public")
-    }
-});
-
-// client
-mix.js("resources/js/app-client.js", "public/js");
-
-// server
-mix.js("resources/js/app-server.js", "public/js");
-
-if (process.env.npm_lifecycle_event !== "hot") {
-    mix.options({
-        extractVueStyles: true
-    }).version();
-}
